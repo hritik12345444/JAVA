b7c926eb39305ecd1d057095819031d170a5b6e0
hritik12345444 <hritik12345444@gmail.com>
2025-06-30 17:21:04 +0530

    1st pattern * * * * * * * * * *

---
+++ Lecture_1_to_5/loops/pattern/pattern.java
@@ -0,0 +1,21 @@
+// 1. pattern
+package Lecture_1_to_5.loops.pattern;
+
+import java.util.*;
+public class pattern {
+    public static void main(String []arg){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter n : ");
+
+        int n = sc.nextInt();
+
+        for(int i= 1; i<=n; i++){
+            for(int j = 0 ; j<i; j++){
+                System.err.print(" * ");
+            }
+            System.out.println();
+        }
+        sc.close();
+        
+    }
+}
