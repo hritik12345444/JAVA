96e5289fe08624fa22ff9d2f3987a5155bb8ee94
hritik12345444 <hritik12345444@gmail.com>
2025-06-30 10:57:52 +0530

    variables in java

---
+++ variable.java
@@ -0,0 +1,19 @@
+// Declare meaningful variables of each type 
+public class variable {
+    public static void main(String [] args){
+        int num = 3;
+        double percent = 44.5;
+        String name = "Hritik";
+        char lete = 'a';
+        boolean adult = true;
+
+        // Outputs  
+        System.out.println(num );
+        System.out.println(percent);
+        System.out.println(name);
+        System.out.println(lete);
+        System.out.println(adult);
+        
+
+    }
+}
