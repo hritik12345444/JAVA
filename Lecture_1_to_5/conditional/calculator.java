69d612ea863cc19a834e63b1c599083afcd2062f
hritik12345444 <hritik12345444@gmail.com>
2025-06-30 12:01:01 +0530

    calculator

---
+++ Lecture_1_to_5/conditional/calculator.java
@@ -0,0 +1,30 @@
+//  Make a calculator. Take 2 number (a & b) from the user and an operations like + - * /
+package Lecture_1_to_5.conditional;
+
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[]arg){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number : ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number : ");
+        int b = sc.nextInt();
+        System.out.print("Enter a opertor : ");
+        char ch = sc.next().charAt(0);  // phla word lo uska 0th character utha lo
+
+        if(ch == '+'){
+            System.out.println("Sum : "+(a + b));
+        }else if (ch == '-'){
+            System.out.println("Sub : "+(a - b));
+        }
+        else if (ch == '*'){
+            System.out.println("Mul : "+(a*b));
+        }else if(ch == '/'){
+            System.out.println("Div : " + (a / b));
+        }else{
+            System.out.println("Invalid operator ");
+        }
+        sc.close();
+    }
+}
