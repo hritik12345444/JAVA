96cdaed6547e2b3b5cbfb57db06c2a85ee0a612d
hritik12345444 <hritik12345444@gmail.com>
2025-07-01 11:52:30 +0530

    pattern 9

---
+++ Lecture_1_to_5/loops/pattern/pattern9.java
@@ -0,0 +1,29 @@
+// pattern 9
+// 1
+// 2 3 
+// 4 5 6
+// 7 8 9 10
+// 11 12 13 14 15
+package Lecture_1_to_5.loops.pattern;
+
+import java.util.Scanner;
+
+public class pattern9 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n : ");
+        int n= sc.nextInt();
+        int j=1;
+        for(int i= 1; i<=n; i++){
+            int k =1;
+            while(k<=i){
+                System.out.print(j + " ");
+                j++;
+                k++;
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
