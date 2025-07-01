27d3fc2abf12c739643b5e5ca4aae696e6c033bf
hritik12345444 <hritik12345444@gmail.com>
2025-07-01 11:43:58 +0530

    pattern 8

---
+++ Lecture_1_to_5/loops/pattern/pattern8.java
@@ -0,0 +1,25 @@
+// pattern 8
+// 1 2 3 4 5 
+// 1 2 3 4 
+// 1 2 3
+// 1 2 
+// 1 
+package Lecture_1_to_5.loops.pattern;
+
+import java.util.Scanner;
+
+public class pattern8{
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter n : ");
+        int n= sc.nextInt();
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.err.print(j +" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
