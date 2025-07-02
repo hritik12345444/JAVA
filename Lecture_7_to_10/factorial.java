4833b274b73c068aec6205d2b0704a38d9c2a72b
hritik12345444 <hritik12345444@gmail.com>
2025-07-02 21:38:13 +0530

    factorial

---
+++ Lecture_7_to_10/factorial.java
@@ -0,0 +1,23 @@
+package Lecture_7_to_10;
+
+import java.util.Scanner;
+
+public class factorial {
+    public static int factorialOfNum(int num){
+        int res =1;
+        for(int i=1; i<=num; i++){
+            res *= i;
+        }
+        return res;
+    }
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter num : ");
+        int num = sc.nextInt();
+
+        int fact = factorialOfNum(num);
+        System.out.println("factorial : " + fact);
+        sc.close();
+    }
+}
