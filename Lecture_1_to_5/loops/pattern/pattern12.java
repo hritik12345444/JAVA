48037006371a6586be240225f02bd79ebd448d9b
hritik12345444 <hritik12345444@gmail.com>
2025-07-02 14:52:10 +0530

    pattern 12

--- Lecture_1_to_5/loops/pattern/pattern12.java
+++ Lecture_1_to_5/loops/pattern/pattern12.java
@@ -1,5 +1,31 @@
+// pattern 12
+//     *****
+//    *****
+//   *****
+//  *****
+// *****
 package Lecture_1_to_5.loops.pattern;
 
+import java.util.*;
+
 public class pattern12 {
-    
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter n : ");
+        int n = sc.nextInt();
+
+        for(int i= n; i>=1; i--){
+            for(int j=1;j<i; j++){
+                System.err.print(" ");
+            }
+            for(int j= 1; j<=n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+        sc.close();
+    }
 }
