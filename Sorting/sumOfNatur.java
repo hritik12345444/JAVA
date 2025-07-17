f0f6467e5ce75d26c18ac0b085a72e6143c19ca6
hritik12345444 <hritik12345444@gmail.com>
2025-07-17 14:51:41 +0530

    sum of n natural number

---
+++ Sorting/sumOfNatur.java
@@ -0,0 +1,27 @@
+// print sum of first n natural numbers
+package Sorting;
+
+import java.util.Scanner;
+public class sumOfNatur {
+
+    static int sum = 0;
+    public static void sumOfNatural(int n){
+
+        if(n==0){
+            System.out.println("sum of first n natural number is : " + sum);
+            return;
+        }
+        sum = sum + n;
+        sumOfNatural(n-1);
+    }
+
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n : ");
+        int n = sc.nextInt();
+
+        sumOfNatural(n);
+        sc.close();
+    }
+}
